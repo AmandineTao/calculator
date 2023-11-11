e6ec4acb0c7952e50151abe3fc297904d4a5b2de
AmandineTao <AmandineTao@users.noreply.github.com>
2023-11-11 15:07:25 +0000

    deploy: 90bfae70fcc47ace6f18ff47b0681b077e7e03d7

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let inputDisplay = document.getElementById('inputDisplay');
 let resultDisplay = document.getElementById('resultDisplay');
 let poweredOn = true;
 
+
 function appendToDisplay(value) {
     if (poweredOn) {
         inputDisplay.value += value;
@@ -27,46 +28,53 @@ function clearDisplay() {
     }
 }
 
+function evalValue(val) {
+    return eval(val.replace(/\^/g, "**"));
+}
+
 function calculate() {
     if (poweredOn) {
         try {
-            resultDisplay.value = eval(inputDisplay.value);
+            resultDisplay.value = evalValue(inputDisplay.value);
         } catch (error) {
-            resultDisplay.value = 'Error';
+            resultDisplay.value = 'Error: check operation';
         }
     }
 }
 
+
 function computePower() {
     if (poweredOn) {
-        inputDisplay.value += "**";
+        inputDisplay.value += "^";
     }
 }
 
 function convertToBinary() {
     if (poweredOn) {
-        let decimalValue = parseInt(inputDisplay.value, 10);
+        let decimalValue = parseInt(evalValue(inputDisplay.value), 10);
         resultDisplay.value = decimalValue.toString(2);
     }
 }
 
 function convertToHexa() {
     if (poweredOn) {
-        let decimalValue = parseInt(inputDisplay.value, 10);
+        let decimalValue = parseInt(evalValue(inputDisplay.value), 10);
         resultDisplay.value = decimalValue.toString(16);
     }
 }
 
 function convertToOctal() {
     if (poweredOn) {
-        let decimalValue = parseInt(inputDisplay.value, 10);
+        let decimalValue = parseInt(evalValue(inputDisplay.value), 10);
         resultDisplay.value = decimalValue.toString(8);
     }
 }
 
 function powerCalc() {
     poweredOn = !poweredOn;
-    if (!poweredOn) {
+    if (poweredOn) {
+        resultDisplay.value = '0'; // Add '0' to inputDisplay when turning on the calculator
+    } else {
         inputDisplay.value = '';
         resultDisplay.value = '';
     }
